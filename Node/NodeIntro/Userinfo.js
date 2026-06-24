16a59ab4a8df6d50c33fc5966870992dd101a326
Soyab-pinjari <pinjarisoyab2006@gmail.com>
2026-06-24 13:33:46 +0530

    Refactor user info console log output
    Modified console log formatting for user information.

---
+++ Node/NodeIntro/Userinfo.js
@@ -0,0 +1,12 @@
+
+const name = "Soyeb";
+const age = 19;
+const favoriteApp = "WhatsApp";
+
+console.log(`Name : ${name}`);
+console.log(`Age  : ${age}`);
+console.log(`Favorite App : ${favoriteApp}`);
+
+console.log(
+  `\nMy name is ${name}, I am ${age} years old, and my favorite app is ${favoriteApp}.`
+);
