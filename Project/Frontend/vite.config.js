4fafde89e383bfcc8c5b9417d7c58e5a1cd859d8
Soyab-pinjari <pinjarisoyab2006@gmail.com>
2026-06-24 10:25:38 +0530

    Add files via upload

---
+++ Project/Frontend/vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'  // Correct import
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],  // Add tailwindcss() here
+})
